985add5c5e150ba23a56c1ccfb20545eefce17a9
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-05-20 10:51:36 +0400

    Add new solved tasks

---
+++ Collatz Conjecture/solution.ts
@@ -0,0 +1,20 @@
+export namespace CollatzConjecture {
+	export function steps(count: number): number {
+		if (count <= 0) {
+			throw new Error('Only positive numbers are allowed');
+		}
+
+		let steps = 0;
+
+		while (count !== 1) {
+			if (count % 2 === 0) {
+				count /= 2;
+			} else {
+				count = 3 * count + 1;
+			}
+			steps++;
+		}
+
+		return steps;
+	}
+}
---
+++ Scrabble score/solution.ts
@@ -0,0 +1,25 @@
+export namespace ScrabbleScore {
+	const tiles: Map<number, string[]> = new Map([
+		[1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']],
+		[2, ['D', 'G']],
+		[3, ['B', 'C', 'M', 'P']],
+		[4, ['F', 'H', 'V', 'W', 'Y']],
+		[5, ['K']],
+		[8, ['J', 'X']],
+		[10, ['Q', 'Z']],
+	]);
+
+	function getValue(letter: string): number {
+		for (const [value, letters] of tiles) {
+			if (letters.includes(letter)) {
+				return value;
+			}
+		}
+		throw new Error(`Unknown letter: ${letter}`);
+	}
+
+	export function score(letters: string = ''): number {
+		return Array.from(letters.toUpperCase())
+			.reduce((score, letter) => score + getValue(letter), 0);
+	}
+}
